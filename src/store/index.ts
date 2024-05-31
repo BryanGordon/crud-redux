8daf4a8bd2b4789196007991bbf5c261139f4e6d
BryanGordon <bryanbge92@gmail.com>
2024-05-31 14:45:14 -0500

    Update redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersSlice from './users/slice'
 
+const persistenceLocalStorageMiddleware = (store) => (next) => (action) => {
+  next(action)
+  localStorage.setItem('_redux_state', JSON.stringify(store.getState()))
+}
+
 export const store = configureStore({
   reducer: {
     users: usersSlice
-  }
+  },
+  middleware: [persistenceLocalStorageMiddleware]
 })
 
 export type RootState = ReturnType<typeof store.getState>
